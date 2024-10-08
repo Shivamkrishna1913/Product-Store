3dc169431957ac1aad7d6d7dd62dc6e2ceb9eab6
shiva <137472627+Shivamkrishna1913@users.noreply.github.com>
2024-10-08 19:39:08 +0530

    ..

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
 import path from "path";
+
+import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.routes.js";
+
 dotenv.config();
+
 const app = express();
 app.use(express.json());
 
