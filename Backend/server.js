46e0d1ec397ac907fc3f94b2c17a4fe58ba0a24e
shiva <137472627+Shivamkrishna1913@users.noreply.github.com>
2024-10-09 16:04:32 +0530

    ..

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.routes.js";
 
@@ -9,18 +9,24 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:5173/",
+};
+
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5000;
 
 app.use("/api/products", productRoutes);
-
-const __dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+// const NODE_ENV = "production";
+// const __dirname = path.resolve();
+// if (NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 app.listen(5000, () => {
   connectDB();
   console.log("Server running at http://localhost:" + PORT);
